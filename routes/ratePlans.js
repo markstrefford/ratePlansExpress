bb35251cfb35411f4191a5c78a1207f3c6fb280b
Mark Strefford <markstrefford@me.com>
2014-04-09 12:02:09 +0100

    Saving / retrieving rate plans + R&A in Couchbase

--- routes/ratePlans.js
+++ routes/ratePlans.js
@@ -3,52 +3,64 @@
  * Created by markstrefford on 02/04/2014.
  */
 
-var _ = require('underscore');
+var _ = require('underscore'),
+    moment = require('moment');
 //      request = require('request');
+
+/**
+ *
+ * {"query":{"bool":{"must":[{"range":{"couchbaseDocument.doc.EffectiveDate":{"gte":"2014-03-03"}}},{"range":{"couchbaseDocument.doc.ExpireDate":{"lte":"2014-03-04"}}}],"must_not":[],"should":[]}},"from":0,"size":50,"sort":[],"facets":{}}
+ *
+ * /
 
 module.exports = function (ratePlanDb, rateAvailDb, app) {
     /**
      * Saving rateplan stuff
      */
-    app.post('/rateplans/pulled/:rateplanId', function (req, res) {
+    app.post('/rateplans/pulled', function (req, res) {
         var rateplan = req.body;
-        var rateplan_id = req.params.rateplanId;
 
-        savePullRatePlan(rateplan_id, rateplan, function (error, result) {
+        savePullRatePlan(rateplan, function (error, srpResult) {
             if (error) res.send(500);
             else res.send(200);
         })
     });
 
-    var savePullRatePlan = function (rateplan_id, rateplan, callback) {
-        ratePlanDb.set(rateplan_id, rateplan, function (error, srpResult) {
-            console.log("savePullRatePlan:" + JSON.stringify(srpResult));
+    var savePullRatePlan = function (rateplan, callback) {
+        var brandCode = rateplan.OTA_HotelAvailRS.RoomStays.RoomStay.BasicPropertyInfo.BrandCode;
+        var hotelCode = rateplan.OTA_HotelAvailRS.RoomStays.RoomStay.BasicPropertyInfo.HotelCode;
+        var ratePlanCode = rateplan.OTA_HotelAvailRS.RoomStays.RoomStay.RatePlans.RatePlan.RatePlanCode;
+        var key = brandCode + "::" + hotelCode + "::" + ratePlanCode;
+
+        console.log("savePullRatePlan: " + key);
+        ratePlanDb.set(key, rateplan, function (error, srpResult) {
             if (error) callback(error);
-            else //{       // TODO - Move to push rates!!
-                //console.log("Calling saveRatesAndAvailability " + JSON.stringify(rateplan));
-                //saveRatesAndAvailability(rateplan_id, rateplan, function (error, sraResult) {
-                //    if (error) callback(error)
-                //    else callback(null, sraResult);
-                //})
-                //}
+            else {
+                console.log("Calling saveRatesAndAvailability " + key);
+                saveRatesAndAvailability(key, rateplan, function (error, sraResult) {
+                    if (error) callback(error)
+                    else callback(null, sraResult);
+                })
+            }
             callback(null, srpResult);
         })
     }
 
     // saveRatesAndAvailability for push providers
-    var saveRatesAndAvailability = function (rateplan_id, rateplan, callback) {
-        console.log("saveRatesAndAvailability starting..." + JSON.stringify(rateplan));
+    var saveRatesAndAvailability = function (ratePlanKey, rateplan, callback) {
         var meta = {};                                        // TODO - Set expiry!!
-        var hotelId = rateplan.OTA_HotelAvailRS.RoomStays.RoomStay.BasicPropertyInfo.HotelCode,
-            roomRate = rateplan.OTA_HotelAvailRS.RoomStays.RoomStay.RoomRates.RoomRate;
-        //
-        // for (var rr in roomRate) {
+        var roomRate = rateplan.OTA_HotelAvailRS.RoomStays.RoomStay.RoomRates.RoomRate;
+        // Iterate through room rates and save individually
         _.each(roomRate, function (rr) {
-            var effectiveDate = rr.Rates.Rate.EffectiveDate;         // TODO - Assume this format works as is!!!   May need LOS as well??
-            var key = hotelId + '::' + rateplan_id + '::' + effectiveDate;
-            console.log("Writing rate " + key);
-            rateAvailDb.set(key, rr, meta, function (error, sraResult) {
-                    console.log("saveRatesAndAvailability:" + sraResult);
+            // TODO - Write a create key function
+            // TODO - Key probably needs occupancy as well!
+            var bookingCode = rr.BookingCode;
+            var roomTypeCode = rr.RoomTypeCode;
+            var effectiveDate = rr.Rates.Rate.EffectiveDate;
+            var expireDate = rr.Rates.Rate.ExpireDate;
+            var rateAvailKey = ratePlanKey + "::" + bookingCode + "::" + roomTypeCode + "::" + effectiveDate + "::" + expireDate;
+            console.log("Writing rate " + rateAvailKey);
+            rateAvailDb.set(rateAvailKey, rr, meta, function (error, sraResult) {
                     if (error) callback(error);
                     else callback(null, sraResult);
                 }
