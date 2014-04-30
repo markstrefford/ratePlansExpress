cabbe8d93c07b68d385ce23f587381cf7c1959d5
Mark Strefford <markstrefford@me.com>
2014-04-30 16:52:17 +0100

    Initial working version of csv reader for Liberate

--- processAifOutFile.js
+++ processAifOutFile.js
@@ -19,7 +19,7 @@ var express = require('express'),
 // Dev / prod config options
 var config = {
     dbHost: 'localhost:8091',
-    outFile: '/Users/markstrefford/Downloads/5.out'
+    outFile: '5-sample.out'
 };
 
 
@@ -39,6 +39,36 @@ var createKey = function () {
     return key;
 }
 
+var writeToDb = function (key, roomRef, roomRate, callback) {
+    var rr,
+        cas
+        ;
+    rateAvailDb.get(key, function (error, result) {
+        //console.log(error.code);
+        if (error) {
+            console.log("1) Key " + key + " doesn't exist, creating...");
+            cas = {};  rr={};
+            rr[roomRef]=roomRate;         // TODO - try to by DRY here!
+        } else {
+            console.log("2) Already exists: " + key + " : " + JSON.stringify(result));
+            rr = result.value;
+            cas = result.cas;
+            rr[roomRef]=roomRate;         // TODO - try to by DRY here!
+        }
+        console.log("3) Writing : " + key + " : " + JSON.stringify(rr));
+
+        // Now write to the DB
+        // TODO - Work through CAS checking...
+        rateAvailDb.set(key, rr, cas, function (error, result) {
+            if (error) callback(error);
+            else {
+                console.log("4) " + key + " written OK");
+                callback(result);
+            }
+        });
+    });
+}
+
 // TODO - Fix why fast-csv doesn't handle delimeters properly!!
 var stream = fs.createReadStream(config.outFile);
 csv
@@ -56,52 +86,35 @@ csv
         var numChildren = 0; // TODO - Check what the data looks like here!
         // TODO - Validate key and data is what we actually need here!
         // TODO - Validate if we can reduce the JSON document size easily too
-        var key = createKey(data.hotel, data.checkIn, data.checkOut, numAdults, numChildren);
+        var key = createKey(data.hotel, data.checkIn, data.checkOut, numAdults, numChildren); //, data.roomType, data.board, data.contractName);
         var roomRef = createKey(data.roomType, data.board, data.contractName);
         // TODO - Get a document with this key first (if it exists)
-        roomRate = {};
-        //console.log("Checking for " + key);
-        rateAvailDb.get(key, function (error, result) {
-            if (error) {
-                console.log("Key " + key + " doesn't exist, creating...");
-            } else {
-                console.log("Key " + key + " already exists, updating...");
-                roomRate = result.value;
-                //console.log(roomRate);
+        var roomRate = {
+            rc: {
+                cn: data.contractName,
+                cl: data.classification,
+                de: data.destination
+            },
+            pr: {
+                cu: data.currency,
+                pr: data.price,
+                sp: data.sellingPrice,
+                np: data.netPrice,
+                a: data.allotment
+            },
+            so: data.promotion,
+            oc: {
+                a: numAdults,
+                c: numChildren
+            },
+            cn: data.cancellationPolicy,
+        };
+        writeToDb(key, roomRef, roomRate, function (error, result) {
+            if (error) console.log(error)
+            else {
+                console.log("5) Document " + key + " callback with OK");
+                callback(result);
             }
-            //console.log(result);
-
-        });
-
-        // Create new roomRate entry
-        var rr = {
-            rateCode: {
-                contractName: data.contractName,
-                classification: data.classification,
-                destination: data.destination
-            },
-            price: {
-                currency: data.currency,
-                price: data.price,
-                sellingPrice: data.sellingPrice,
-                netPrice: data.netPrice,
-                allotment: data.allotment
-            },
-            promotion: data.promotion,
-            occupancy: {
-                adults: numAdults,
-                children: numChildren
-            },
-            cancellation: data.cancellationPolicy,
-        }
-        console.log(key + ":" + JSON.stringify(roomRate));
-        roomRate[roomRef] = rr;
-
-        // Now write to the DB
-        // TODO - Work through CAS checking...
-        rateAvailDb.set(key, roomRate, function(error, result) {
-            if (error) callback(error)
-            else console.log("Doc " + key + " written OK");
         });
 
     })
