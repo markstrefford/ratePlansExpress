91a69a9821a73299454bd44bd06e764787789beb
Mark Strefford <markstrefford@me.com>
2014-08-01 15:57:41 +0100

    Additional logging

--- serverExpress4.js
+++ serverExpress4.js
@@ -147,6 +147,7 @@ var getOTA2004bRates = function (req, res, next) {
     // Gets docs from Couchbase
     ota2004Db.get('10000', {format:'raw'}, function(err, res) {
         console.log(res.value.toString('hex'));
+        console.log(res.value);
 
     //ota2004Db.getMulti(_.flatten([request.hotelId, rateDocKeys]), {format: 'json'}, function (err, results) {
     //ota2004Db.getMulti('10000', {format: 'json'}, function (err, results) {
